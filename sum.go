2bebd41c4010fcac2dec29bf3e222e8b67e9f8b6
Cristiano Rosa <cristiano@inverter.com.br>
2022-05-04 17:22:15 -0400

    feat: Add sum function

---
+++ sum.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(2, 2))
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func times(a int, b int) int {
+	return a * b
+}
+
+func sumX(a int, b int) int {
+	return a + b + a
+}
+
+func sumY(a int, b int) int {
+	return a - b + a
+}
+
+func sumt(a int, b int) int {
+	return a - b + a
+}
